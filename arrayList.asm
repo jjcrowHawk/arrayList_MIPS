e7985d7ae10528209aab5f06252a93827612dac1
jjcrowHawk <jjcrow@espol.edu.ec>
2018-06-19 00:53:52 -0500

    Implementacion para poder regresar a la funcion main() en menu() realizada

--- arrayList.asm
+++ arrayList.asm
@@ -6,10 +6,11 @@
 ch:		.word	0	#variable that stores the return value of menu() functions
 element:	.word	0	#variable that stores the input value of user for new element
 pos:		.word	0	#varaible that stores the input value of user that define the position of the element to delete
-
+	
+	.text
 main:	
 menu_loop:	jal menu
-		$v0,10
+		li $v0,10
 		syscall
 
 
@@ -19,14 +20,18 @@ DECORATOR:	.asciiz	"\n\t\t********************************************\n"
 TITLE_MENU:	.asciiz "\t\t******LIST Implementation Using Arrays******\n"
 BODY_MENU:	.asciiz	"\t1. Create\n\t2. Insert\n\t3. Delete\n\t4. Count\n\t5. Find\n\t6. Display\n\t7.Exit\n\n\tEnter your choice : "
 	.text
-menu:	la $a0,DECORATOR	#set argument to print
+menu:	sub $sp,$sp,4		#reserve 4 bytes on stack
+	sw $ra,($sp) 		#copy return address to reserved stack memory place
+	la $a0,DECORATOR	#set argument to print
 	jal print		#call print function
 	la $a0,TITLE_MENU	#set argument to print
 	jal print		#call print function
 	la $a0, BODY_MENU	#set argument to print
 	jal print		#call print function
+	lw $ra,($sp)		#load the return adress of main function to $ra
+	addi $sp,$sp,4		#restore the stack pointer
 	jr $ra			#return (end of function) // AQUI HAY QUE GUARDAR LA DIRECCION, YA QUE SE PIERDE el return Adrress AL USAR PRINT
 
-print:	li	$v0,4		#Set value for print a String
+print:	li $v0,4		#Set value for print a String
 	syscall			#call system
 	jr $ra			#return (end of function)
